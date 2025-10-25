5331e351672393ed6b3a892ca4a155dbbb718bb2
Maximiliano Opitz Turra <mopitz199@gmail.com>
2025-10-24 22:02:11 -0300

    working with themes

---
+++ colors.tsx
@@ -0,0 +1,23 @@
+export const colors = {
+  light: {
+    primary: "#0057FF",
+    background: "#F9F9F9",
+    onBackground: "#1D2430",
+    surface: "#FFFFFF",
+    onSurface: "#1D2430",
+    error: "#FF5A5F",
+    success: "#00C48C",
+    separatorLine: "#CACACA",
+  },
+  dark: {
+    primary: "#0057FF",
+    background: "#1D2430",
+    onBackground: "#F9F9F9",
+    onBackgroundSecondary: "#D7D7D7",
+    surface: "#2A3447",
+    onSurface: "#F9F9F9",
+    error: "#FF5A5F",
+    success: "#00C48C",
+    separatorLine: "#536689",
+  },
+};
--- components/MainView.tsx
+++ components/MainView.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, View } from "react-native";
+import { colors } from "@/colors";
+import { StyleSheet, useColorScheme, View } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export interface MainViewProps {
@@ -6,7 +7,16 @@ export interface MainViewProps {
   children: React.ReactNode;
 }
 
+type StyleParams = {
+  isDarkMode: boolean;
+};
+
 export default function MainView({ headerShown = true, children }: MainViewProps) {
+
+  const theme = useColorScheme();
+  const isDarkMode = theme === 'dark';
+  const style = makeStyles({ isDarkMode });
+
   if (headerShown) {
     return (
       <View style={style.container}>
@@ -22,10 +32,13 @@ export default function MainView({ headerShown = true, children }: MainViewProps
   }
 }
 
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    borderColor: 'red',
-    borderWidth: 2,
-  },
-})
+function makeStyles({ isDarkMode }: StyleParams) {
+  console.log("isDarkMode", isDarkMode);
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: isDarkMode ? colors.dark.background : colors.light.background,
+      borderWidth: 2,
+    },
+  });
+}
