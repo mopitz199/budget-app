532b19b8be1b95f14af979420cb8b609141cb811
Maximiliano Opitz Turra <mopitz199@gmail.com>
2025-11-16 14:54:57 -0300

    starting to test auth

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,9 @@ import { PrincipalButton } from "@/components/Buttons";
 import MainView from "@/components/MainView";
 import { useHeaderBehavior } from "@/hooks/header-behavior";
 import { ScreenConf } from "@/types/screen-conf";
+import { getAuth, onAuthStateChanged } from '@react-native-firebase/auth';
 import { useRouter } from "expo-router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useColorScheme } from "react-native";
 
 export default function Index() {
@@ -14,6 +15,21 @@ export default function Index() {
   const theme = useColorScheme();
   
   useHeaderBehavior({ headerShown: screenConf.headerShown });
+
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function handleAuthStateChanged(user: any) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(getAuth(), handleAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
 
   return (
     <MainView headerShown={screenConf.headerShown}>
