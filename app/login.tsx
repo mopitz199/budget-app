93d38449e435fd92aaeb21755babae8f65cc06da
Maximiliano Opitz Turra <mopitz199@gmail.com>
2025-11-16 22:47:06 -0300

    adding the scrollview for better experience in the login part

--- app/login.tsx
+++ app/login.tsx
@@ -9,7 +9,7 @@ import { ScreenConf } from "@/types/screen-conf";
 import { useRouter } from "expo-router";
 import { useState } from "react";
 import { useTranslation } from 'react-i18next';
-import { TouchableOpacity, View } from "react-native";
+import { ScrollView, TouchableOpacity, View } from "react-native";
 
 export default function LoginScreen() {
   const screenConf: ScreenConf = {
@@ -31,35 +31,37 @@ export default function LoginScreen() {
 
   return (
     <MainView headerShown={screenConf.headerShown}>
-      <View style={{ flex: 1, borderWidth: 0, paddingTop: '20%' }}>
-        <Title style={{ alignSelf: 'center', marginBottom: 40 }}>{t('isTimeForSaving')}</Title>
-        <Input
-          textInputProps={{
-            value: email,
-            onChangeText: setEmail,
-            placeholder: t('enterYourEmail'),
-          }}
-          //errorMessage="The amount must be greater than zero"
-          labelMessage={t('email')}
-        />
-        <PasswordInput
-          style={{marginTop: 20}}
-          value={password}
-          onChangeValue={setPassword}
-          placeholder={t('enterYourPassword')}
-          //errorMessage="The amount must be greater than zero"
-          labelMessage={t('password')}
-        />
-        <PrincipalButton style={{ marginTop: 40 }} title={t('logIn')} onPress={handleLogin} />
-        <TouchableOpacity onPress={() => {router.navigate('/recover-account')}}>
-          <Text style={{ marginTop: 10, alignSelf: 'flex-end' }}>{t('forgotPassword')}</Text>
-        </TouchableOpacity>
-        <LoginLine style={{marginVertical: 10}}>or</LoginLine>
-        <GoogleButton title={t('continueWithGoogle')} onPress={() => {router.replace('/login')}}/>
-        <View style={{ flexDirection: 'row', justifyContent: 'center', gap: 8, marginTop: 10 }}>
-          <Text>{t("doesNotHaveAnAccount")}</Text>
-          <LinkText onPress={() => {router.navigate('/register')}}>{t("signUp")}</LinkText>
-        </View>
+      <View style={{ flex: 1, borderWidth: 0}}>
+        <ScrollView style={{ paddingTop: '20%' }}>
+          <Title style={{ alignSelf: 'center', marginBottom: 40 }}>{t('isTimeForSaving')}</Title>
+          <Input
+            textInputProps={{
+              value: email,
+              onChangeText: setEmail,
+              placeholder: t('enterYourEmail'),
+            }}
+            //errorMessage="The amount must be greater than zero"
+            labelMessage={t('email')}
+          />
+          <PasswordInput
+            style={{marginTop: 20}}
+            value={password}
+            onChangeValue={setPassword}
+            placeholder={t('enterYourPassword')}
+            //errorMessage="The amount must be greater than zero"
+            labelMessage={t('password')}
+          />
+          <PrincipalButton style={{ marginTop: 40 }} title={t('logIn')} onPress={handleLogin} />
+          <TouchableOpacity onPress={() => {router.navigate('/recover-account')}}>
+            <Text style={{ marginTop: 10, alignSelf: 'flex-end' }}>{t('forgotPassword')}</Text>
+          </TouchableOpacity>
+          <LoginLine style={{marginVertical: 10}}>or</LoginLine>
+          <GoogleButton title={t('continueWithGoogle')} onPress={() => {router.replace('/login')}}/>
+          <View style={{ flexDirection: 'row', justifyContent: 'center', gap: 8, marginTop: 10 }}>
+            <Text>{t("doesNotHaveAnAccount")}</Text>
+            <LinkText onPress={() => {router.navigate('/register')}}>{t("signUp")}</LinkText>
+          </View>
+        </ScrollView>
       </View>
     </MainView>
   );
