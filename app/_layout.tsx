9a06a8c8e004ed8eeace207d83b4ece9368df4c9
Maximiliano Opitz Turra <mopitz199@gmail.com>
2025-12-16 22:28:09 -0300

    upading the react native firebase library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,34 @@
+import { getAuth } from "@react-native-firebase/auth";
+import { doc, getDoc, getFirestore } from '@react-native-firebase/firestore';
 import { Stack } from "expo-router";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function RootLayout() {
+
+  const { t } = useTranslation();
+  const auth = getAuth()
+  
+  const [userSettings, setUserSettings] = useState<any | null>(null);
+
+  const fetchUserSettings = async () => {
+    const user = auth.currentUser;
+    if(!user){
+      // error
+    }else {
+      const db = getFirestore();
+      const docRef = doc(db, "user_settings", user.uid);
+      const docSnap = await getDoc(docRef); 
+
+      if(docSnap.exists()){
+        const userSettings = docSnap.data();
+        return userSettings;        
+      }else{
+        // error
+      }
+    }
+  }
+
   return (
     <Stack screenOptions={{animation: 'none'}}>
       <Stack.Screen name="login" options={{animation: 'none'}}/>
